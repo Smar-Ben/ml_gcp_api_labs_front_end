4bc8514a83bce0fc5796ea7533e10acc695915a6
Smar-Ben <benrams1997@gmail.com>
2022-12-06 20:39:53 +0100

    fix: remove unneccessary variable

--- src/component/result/Entities.js
+++ src/component/result/Entities.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
-import Tooltip from "react-bootstrap/Tooltip";
+import Help from "../../img/help.svg";
 
 const Entities = (props) => {
-  console.log(props.responseApi);
   return (
     <Table
       striped
@@ -17,7 +15,7 @@ const Entities = (props) => {
     >
       <thead>
         <tr>
-          <th>Entité </th>
+          <th>Entité</th>
           <th>Type</th>
           <th>Score</th>
           <th>Wiki</th>
@@ -35,17 +33,6 @@ const Entities = (props) => {
               )}
             </td>
           </tr>
-          // <ListGroup.Item as="li">
-          //   Le mot {entity.name} est un {entity.type} avec une confidence de{" "}
-          //   {Math.round(entity.score * 100)}%<br />
-          //   <b>Langue detecté</b> : {entity.language}
-          //   <br />
-          //   {entity.wikipedia_url.length > 0 && (
-          //     <span>
-          //       <b>Lien vers wikipédia</b> : {entity.wikipedia_url}
-          //     </span>
-          //   )}
-          // </ListGroup.Item>
         ))}
       </tbody>
     </Table>
